5f4aae0c061f45e7d111311ecd43d33a742ee4ba
Abhishek Jain <abhishekjai35@gmail.com>
2021-07-04 20:17:34 +0530

    Added migrations and update form

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import sys
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
 
   def __repr__(self):
     return f'<Todo {self.id}, {self.description}'
 
-db.create_all()
-
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.form.get('description', '')
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return redirect(url_for('index'))
+  error = False
+  body={}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    db.session.rollback()
+    print(sys.exc_info())
+    error = True
+  finally:
+    db.session.close()
+
+  if not error:
+    return jsonify(body)
 
 @app.route("/")
 
